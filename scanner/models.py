73e5e5323bd6833c467f1a908e44b2c61fa5e861
mmanman514-netizen <mmanman514@gmail.com>
2026-02-02 16:06:06 +0200

    Update models.py

--- scanner/models.py
+++ scanner/models.py
@@ -1,1 +1,24 @@
+# models.py
 
+from typing import List, Optional
+
+
+class ApiResource:
+    def __init__(
+        self,
+        name: str,
+        endpoint: str,
+        methods: List[str],
+        owner_field: Optional[str] = None,
+        sensitive_fields: Optional[List[str]] = None,
+        writable_fields: Optional[List[str]] = None,
+    ):
+        self.name = name
+        self.endpoint = endpoint
+        self.methods = methods
+        self.owner_field = owner_field
+        self.sensitive_fields = sensitive_fields or []
+        self.writable_fields = writable_fields or []
+
+    def __repr__(self):
+        return f"<ApiResource {self.name} {self.endpoint}>"
